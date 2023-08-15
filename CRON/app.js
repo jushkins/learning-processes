fb8da07d177293efdec88bf9f4d9aa1f20ea428f
Jushkin <jushkin.saparmatov@gmail.com>
2023-08-15 17:03:17 +0500

    cron

--- CRON/app.js
+++ CRON/app.js
@@ -1,7 +1,14 @@
 import schedule from "node-schedule";
+import { CronJob } from "cron";
 
-const setDate = new Date("2023-08-15T16:47:00.000+5:00");
+// const setDate = new Date("2023-08-15T16:47:00.000+5:00");
 
-schedule.scheduleJob('*/5 * * * * *', () => {
-  console.log("Job runned");
-});
+// schedule.scheduleJob('*/5 * * * * *', () => {
+//   console.log("Job runned");
+// });
+
+
+const job = new CronJob('* * * * * *', () => {
+  console.log('This shit runs every second', null, true, "Tashkent/Uzbekistan");
+})
+job.start();
